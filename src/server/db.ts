bd41c9383cd7f08322e99c81cf04ae5369292032
liogiladi <liogiladi@gmail.com>
2024-08-02 15:18:48 +0300

    add db types

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,12 +2,13 @@
 
 import { createClient } from "@supabase/supabase-js";
 import assert from "assert";
+import { Database } from "./db.types";
 
 const supabaseUrl = "https://nrvivtkokikskyunzfvr.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 
 assert(supabaseKey, "No supabase key found");
 
-const db = createClient(supabaseUrl, supabaseKey);
+const db = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default db;
--- src/utils/types.ts
+++
@@ -1,6 +0,0 @@
-import { LinkProps } from "next/link";
-
-export type LinkInfo = {
-	name: string;
-	href: LinkProps["href"];
-};
