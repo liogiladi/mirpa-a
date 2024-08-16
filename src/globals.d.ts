c57bbb9850a8c0bfab10ac41a837df555aea6635
liogiladi <liogiladi@gmail.com>
2024-08-16 15:33:45 +0300

    add global typescript utilities types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -3,4 +3,7 @@ export declare global {
 		var isMobile: boolean;
 		var currentPageName: string;
 	}
+
+	type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+	type PropsWithRequiredChildren<P> = P & { children: React.ReactNode };
 }
