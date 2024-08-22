3d87c33184ea10e3f3048440f7b1a76b3121af33
liogiladi <liogiladi@gmail.com>
2024-08-22 15:38:54 +0300

    fix toggle links to be updated with search queries

--- src/app/requested-visits/_components/StatusFilter.tsx
+++ src/app/requested-visits/_components/StatusFilter.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { memo } from "react";
+import { useSearchParams } from "next/navigation";
 import styles from "@/styles/links-filter.module.scss";
+
+import {
+	getUpdatedSearchParamsURL,
+	SEARCH_QUERIES,
+} from "@/utils/searchQueries";
 import ToggleLinks from "@/components/ToggleLinks";
 
 export default memo(function StatusFilter() {
+	const currentParams = useSearchParams();
+
 	return (
 		<div id={styles["links-filter"]}>
 			<ToggleLinks
@@ -11,11 +21,27 @@ export default memo(function StatusFilter() {
 				links={[
 					{
 						name: "בקשות חדשות",
-						href: "/requested-visits?status=pending",
+						href: `/requested-visits?${getUpdatedSearchParamsURL(
+							currentParams,
+							(params) => {
+								params.set(
+									SEARCH_QUERIES.status.name,
+									SEARCH_QUERIES.status.values[0]
+								);
+							}
+						)}`,
 					},
 					{
 						name: "בקשות שנדחו",
-						href: "/requested-visits?status=rejected",
+						href: `/requested-visits?${getUpdatedSearchParamsURL(
+							currentParams,
+							(params) => {
+								params.set(
+									SEARCH_QUERIES.status.name,
+									SEARCH_QUERIES.status.values[1]
+								);
+							}
+						)}`,
 					},
 				]}
 			/>
