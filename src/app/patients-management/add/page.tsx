a38ac47ec74bbc4c86edf7e071931beabfc536a6
liogiladi <liogiladi@gmail.com>
2024-08-26 15:46:05 +0300

    finish

--- src/app/patients-management/add/page.tsx
+++ src/app/patients-management/add/page.tsx
@@ -76,7 +76,6 @@ export default function AddPatient() {
 							toast.success("המטופל נוסף בהצלחה");
 						}
 					} catch (error) {
-						console.error(error);
 						if (!(error as Error).message) {
 							toast.error("תקלה בהוספה");
 						} else toast.error((error as Error).message);
